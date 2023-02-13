f29580a1976049e1ed3a0d580c1b5e7a26c2aff7
Marco Domingues <marco.a.dom78@gmail.com>
2023-02-13 22:52:02 +0000

    controller implementation

--- src/main/java/com/myproj/notepad/Controller.java
+++ src/main/java/com/myproj/notepad/Controller.java
@@ -1,14 +1,126 @@
 package com.myproj.notepad;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-public class Controller {
-    @FXML
-    private Label welcomeText;
+import java.io.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    private FileChooser fileChooser = new FileChooser();
+    private File file;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextArea textpane;
+
+    @FXML
+    private MenuItem newItem;
+
+    @FXML
+    protected void newFile(ActionEvent event) {
+        textpane.clear();
+        Stage stage = (Stage) textpane.getScene().getWindow();
+        stage.setTitle("Untitled - Notepad");
+        file = null;
+    }
+
+    protected void openFile(ActionEvent event) {
+        file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            Stage stage = (Stage) textpane.getScene().getWindow();
+            stage.setTitle(file.getName() + " - Notepad");
+            BufferedReader br = null;
+            try {
+                String sCurrentLine;
+                br = new BufferedReader(new FileReader(file));
+                while ((sCurrentLine = br.readLine()) != null) {
+                    textpane.appendText(sCurrentLine + "\n");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    protected void saveFile(ActionEvent event) {
+        String content = textpane.getText();
+        if (file != null) {
+            try {
+                //if file doesn't exist, then create it
+                if (!file.exists()){
+                    file.createNewFile();
+                }
+                FileWriter fw = new FileWriter(file.getAbsoluteFile());
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.write(content);
+                bw.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else {
+            //open the dialog box
+            file = fileChooser.showSaveDialog(null);
+            if (file!= null){
+                Stage stage = (Stage) textpane.getScene().getWindow();
+                stage.setTitle(file.getName() + " - Notepad");
+                try {
+                    //if doesn't exist, then create it
+                    if (!file.exists()){
+                        file.createNewFile();
+                    }
+                    FileWriter fw  = new FileWriter(file.getAbsoluteFile());
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    bw.write(content);
+                    bw.close();
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @FXML
+    protected void saveAsFile(ActionEvent event) {
+        file = fileChooser.showSaveDialog(null);
+
+        String content = textpane.getText();
+        if (file != null) {
+            Stage stage = (Stage) textpane.getScene().getWindow();
+            stage.setTitle(file.getName() + " - Notepad");
+            try {
+                //if doesn't exist created it
+                if (!file.exists()){
+                    file.createNewFile();
+                }
+                FileWriter fw = new FileWriter(file.getAbsoluteFile());
+                BufferedWriter bw =  new BufferedWriter(fw);
+                bw.write(content);
+                bw.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    protected void exitApp(ActionEvent event) {
+        Platform.exit();
+    }
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
